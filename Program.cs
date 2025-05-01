15ab23dd993d5dc4158c93e7b2aad831c763a7ac
Ahmed Tamer <ahmedtamer2389@gmail.com>
2025-05-01 15:00:32 +0300

    Part 05 - Department Module [Repository]

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 using Demo.DataAccess.Data.Contexts;
+using Demo.DataAccess.Repositories;
+using Demo.Databases.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 using MVC_3.Models;
@@ -12,6 +14,7 @@ namespace MVC_3
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            builder.Services.AddScoped<IDepartmentRepository, DepartmentRepository>();
             builder.Services.AddControllersWithViews();
             builder.Services.AddDbContext<ApplicationDbContext>(Options =>
             {
