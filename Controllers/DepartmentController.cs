a64a559e84ca16a88cae0cbcf3a4229fb1919894
Ahmed Tamer <ahmedtamer2389@gmail.com>
2025-05-01 16:20:33 +0300

    Part 06 - Department Module[Service]

---
+++ Controllers/DepartmentController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Demo.BusinessLogic.services;
+
+namespace MVC_Project.Presentation.Controllers
+{
+    public class DepartmentController(IDepartmentService departmentService) : Controller
+    {
+        
+
+        public IActionResult Index()
+        {
+            var Departments = departmentService.GetAllDepartments();
+            return View();
+        }
+    }
+}
